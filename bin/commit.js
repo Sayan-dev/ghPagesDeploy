de2a728532248d2f3a8016e49ae8e5b1f92bdaa9
Sayan-dev <sayanta2702@gmail.com>
2021-05-28 13:47:32 +0530

    undefined

--- bin/commit.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-var shell = require("shelljs");
-
-shell.exec("git add . && git commit -m 'gh-pages update'");
-      
--- bin/gh-pages.js
+++ bin/gh-pages.js
@@ -4,9 +4,16 @@ var shell = require("shelljs");
 var yargs = require("yargs");
 
 var argv = yargs.usage("$0 command")
-  .command("commit", "commit changes to the repo", function (yargs) {
-    shell.exec("git add -A . && git commit -a -m 'gh-pages update'");
-  })
+  .command("commit", "commit changes to the repo",
+    function (yargs) {
+      return yargs.option('m', {
+        alias: '',
+        describe: 'the commit message'
+      })
+    },
+    function (argv) {
+      shell.exec(`git add -A . && git commit -a -m ${argv.message}`);
+    })
   .command("push", "push changes up to GitHub", function (yargs) {
     shell.exec("git push origin main --force");
   })

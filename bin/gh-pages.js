80dd15445999ba5734862d0b3f9749de83f4f5cc
Sayan-dev <sayanta2702@gmail.com>
2021-05-28 16:59:34 +0530

    Dynamic

--- bin/gh-pages.js
+++ bin/gh-pages.js
@@ -8,18 +8,44 @@ var argv = yargs.usage("$0 command")
     function (yargs) {
       return yargs.option('m', {
         alias: 'message',
+        default: 'new commit',
         describe: 'the commit message'
-      })
+      }).argv
     },
     function (argv) {
       shell.exec(`git add -A . && git commit -a -m ${argv.message}`);
     })
-  .command("push", "push changes up to GitHub", function (yargs) {
-    shell.exec("git push origin main --force");
-  })
-  .command("deploy", "commit and push changes in one step", function (yargs) {
-    shell.exec("ghpages commit && ghpages push");
-  })
+  .command("push", "push changes up to GitHub",
+    function (yargs) {
+      return yargs.option('b', {
+        'b': {
+          alias: 'branch',
+          default: 'main',
+          describe: 'the branch'
+        }
+      }).argv
+    }, function (argv) {
+
+      shell.exec(`git push origin ${argv.b} --force`);
+    })
+  .command("deploy", "commit and push changes in one step",
+    function (yargs) {
+      return yargs.options(
+        {
+          'm': {
+            alias: 'message',
+            default: 'new commit',
+            describe: 'the commit message'
+          },
+          'b': {
+            alias: 'branch',
+            default: 'main',
+            describe: 'the branch'
+          }
+        }).argv
+    }, function (argv) {
+      shell.exec(`ghpages commit -m ${argv.m} && ghpages push -b ${argv.b}`);
+    })
   .demand(1, "must provide a valid command")
   .help("h")
   .alias("h", "help")

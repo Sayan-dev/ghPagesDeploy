e8d8ea77485a4d8a5cc4f3cfa147c3018190e9a5
Sayan-dev <sayanta2702@gmail.com>
2021-05-28 12:18:03 +0530

    gh-pages update

--- bin/gh-pages.js
+++ bin/gh-pages.js
@@ -8,7 +8,7 @@ var argv = yargs.usage("$0 command")
     shell.exec("git add -A . && git commit -a -m 'gh-pages update'");
   })
   .command("push", "push changes up to GitHub", function (yargs) {
-    shell.exec("git push origin master --force");
+    shell.exec("git push origin main --force");
   })
   .command("deploy", "commit and push changes in one step", function (yargs) {
     shell.exec("ghpages commit && ghpages push");
